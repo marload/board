842fafb8348555a837c07225372f8fb93c4e2970
olramde <rladhkstn8@gmail.com>
2019-07-02 22:09:42 +0900

    implement create article

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const url = require('url');
+const qs = require('querystring');
+const fs = require('fs');
 
 const app = http.createServer((req, res) => {
     const currentURL = req.url
@@ -22,11 +24,9 @@ const app = http.createServer((req, res) => {
             </head>
             <body>
                 <h1 align="center">Board</h1>
-                <a href="#">create</a>
+                <a href="create">create</a>
                 <div id="board_list" align="center">
-                    <p>
-                        article list
-                    </p>
+                    ${content}
                 </div>
             </body>
             </html>
@@ -43,22 +43,39 @@ const app = http.createServer((req, res) => {
 
     const templateCreateHTML = () => {
         return `
-            <form action method="https://localhost:3000/create_article">
-                <p>
-                    <input name="title" type="text">
-                </p>
-                <p>
-                    <textarea name="article" cols="50" rows="50"></textarea>
-                </p>
-            </form>
+        <form action="http://localhost:3000/create_article" method="post">
+            <p>
+                <input type="text" name="title" placeholder="title"></p>
+            <p>
+                <textarea name="description" placeholder="article"></textarea>
+            </p>
+            <p>
+                <input type="submit">
+            </p>
+        </form>
         `
     }
 
     if (pathname === '/') {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(templateHTML(templateIndexHTML()));
+    } else if (pathname === '/create') {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(templateHTML(templateCreateHTML()));
     } else if (pathname === '/create_article') {
-    
+        let body = '';
+        req.on('data', (data) => {
+            body = body + data;
+        });
+        req.on('end', () => {
+            const post = qs.parse(body);
+            const title = post.title;
+            const article = post.article;
+            fs.writeFile(`data/${title}`, article, 'utf8', (err) => {
+                res.writeHead(302, {Location: `/`});
+                res.end();
+            })
+        });
     } else {
         const template404Page = `<h1 align='center'>404 Not Found</h1>`
         res.writeHead(404, {"Content-Type": "text/html"});
