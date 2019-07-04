ea1cc1b9fb38da0d360fdba55644ec6a45b90e27
olramde <rladhkstn8@gmail.com>
2019-07-04 17:32:36 +0900

    update aritlce

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = http.createServer((req, res) => {
     const currentURL = req.url
     const pathname = url.parse(currentURL).pathname;
     
-    const templateHTML = (content) => {
+    const templateHTML = (content, pageButton="") => {
         return `
             <!DOCTYPE html>
             <html lang="ko">
@@ -24,7 +24,7 @@ const app = http.createServer((req, res) => {
             </head>
             <body>
                 <h1 align="center"><a href="/">Board</a></h1>
-                <a href="create">create</a>
+                ${pageButton}
                 <div id="board_list" align="center">
                     ${content}
                 </div>
@@ -41,20 +41,30 @@ const app = http.createServer((req, res) => {
         `
     }
 
-    const templateCreateHTML = () => {
+    const templateWriteHTML = (action, title, article, submit) => {
         return `
-        <form action="http://localhost:3000/create_article" method="post">
+        <form action="${action}" method="post">
             <p>
-                <input type="text" name="title" placeholder="title"></p>
+                <input type="text" name="title" placeholder="title" value=${title}></p>
             <p>
-                <textarea name="article" placeholder="article"></textarea>
+                <textarea name="article" placeholder="article">${article}</textarea>
             </p>
             <p>
-                <input type="submit">
+                <input type="submit" value="${submit}">
             </p>
+            <input type="hidden" name="preArticle" value="${title}">
         </form>
         `
     }
+
+    const templateCreateHTML =  () => {
+        return templateWriteHTML("http://localhost:3000/create_article", "", "", "제출");
+    }
+
+    const templateUpdateHTML = (title, article) => {
+        return templateWriteHTML("http://localhost:3000/update_article", title, article, "수정");
+    }
+
 
     if (pathname === '/') {
         fs.readdir("./data", (err, items) => {
@@ -69,7 +79,7 @@ const app = http.createServer((req, res) => {
             articleListHTML += '</ol>'
 
             res.writeHead(200, {"Content-Type": "text/html"});
-            res.end(templateHTML(templateIndexHTML(articleListHTML)));
+            res.end(templateHTML(templateIndexHTML(articleListHTML), "<a href='create'>create</a>"));
         });        
     }
     else if (pathname === '/create') {
@@ -97,14 +107,45 @@ const app = http.createServer((req, res) => {
 
         fs.readFile(`./data/${title}`, 'utf8', (err, article) => {
             if (err) {
-                return;0.
+                return;
             }
 
             const body = `<h2> ${title} </h2> <p> ${article} <p>`
             res.writeHead(200, {"Content-Type": "text/html"});
-            res.end(templateHTML(body));
+            res.end(templateHTML(body, `<a href='create'>create</a> <br> <a href='update-${title}'>update</a>`));
         });
 
+    }
+    else if (pathname.includes("/update_article")) {
+        let body = '';
+        req.on('data', (data) => {
+            body = body + body;
+        });
+        console.log(body);
+        req.on('end', () => {
+            const post = qs.parse(body);
+            const title = post.title;
+            const article = post.article;
+            const preTitle = post.preArticle;
+            // TODO Bug fix
+            fs.writeFile(`data/${title}`, article, 'utf8', (err) => {
+                fs.unlinkSync(`data/${preTitle}`);
+                res.writeHead(302, {Location: `/article-${title}`});
+            });
+            
+        })
+    }
+    else if (pathname.includes("/update-")) {
+        const title = pathname.split("-")[1];
+        fs.readFile(`./data/${title}`, 'utf8', (err, article) => {
+            if (err) {
+                return;
+            }
+
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(templateHTML(templateUpdateHTML(title, article)));
+        });
+        
     }
     else {
         const template404Page = `<h1 align='center'>404 Not Found</h1>`
@@ -113,3 +154,4 @@ const app = http.createServer((req, res) => {
         res.end(template404Page);
     }
 }).listen(3000);
+
